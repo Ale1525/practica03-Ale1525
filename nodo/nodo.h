bda7d4b87636524eb37522e7c4f8f348ac24b312
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-03-24 02:22:10 +0000

    subo nodo.h

--- nodo/nodo.h
+++ nodo/nodo.h
@@ -1,20 +1,13 @@
 #ifndef __NODO_H__
 #define __NODO_H__
 
-/* Declaración incompleta del tipo Nodo (tipo opaco) */
-typedef struct Nodo Nodo;
+typedef struct Nodo{
+    int valor;
+} Nodo;
 
-
-/* Crea un nodo con el valor indicado */
 Nodo* crearNodo(int valor);
-
-/* Obtiene el valor almacenado en el nodo */
-int obtenerValor(Nodo* nodo);
-
-/* Cambia el valor almacenado en el nodo */
-void asignarValor(Nodo* nodo, int valor);
-
-/* Libera la memoria del nodo */
-void destruirNodo(Nodo* nodo);
+int obtenerValor( const Nodo *n);
+void asignarValor(Nodo *n, int valor);
+void destruirNodo(Nodo *n);
 
 #endif
