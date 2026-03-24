0cb050f89790c4694a15e3a59bfecba4e788ce54
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-03-24 03:15:52 +0000

    subo maim.c

--- fraccion/main.c
+++ fraccion/main.c
@@ -1,43 +1,29 @@
 #include <stdio.h>
 #include "fraccion.h"
 
-int main()
-{
-
-    printf("Prueba del TAD Fraccion\n");
-
-    /* TODO
-       1 Crear dos fracciones
-       ejemplo: 1/2 y 3/4
-    */
-    Fraccion* f1 = NULL;
-    Fraccion* f2 = NULL;
-
-
+int main() {
+    Fraccion f1 = crearfraccion(4, 8);
     printf("Fraccion 1: ");
-    /* TODO imprimir */
+    imprimir(&f1);
 
-
+    printf("\n2. Intentando crear una fraccion con den = 0:\n");
+    Fraccion f_error = crearfraccion(5, 0);
     printf("Fraccion 2: ");
-    /* TODO imprimir */
+    imprimir(&f_error);
 
+    printf("\n3. Sumando 1/2 + 1/4 (debe mostrar 3/4):\n");
+    Fraccion a = crearfraccion(1, 2);
+    Fraccion b = crearfraccion(1, 4);
+    Fraccion resultado = suma(&a, &b);
+    printf("Resultado de la suma: ");
+    imprimir(&resultado);
 
-    printf("\nSuma de fracciones:\n");
-
-    /* TODO
-       2 Sumar fracciones
-    */
-    Fraccion* resultado = NULL;
-
-
-    /* TODO
-       3 Imprimir resultado
-    */
-
-
-    /* TODO
-       4 Liberar memoria
-    */
+    printf("\n4. Sumando 1/2 + 1/2 (debe mostrar 1/1):\n");
+    Fraccion c = crearfraccion(1, 2);
+    Fraccion d = crearfraccion(1, 2);
+    Fraccion resultado2 = suma(&c, &d);
+    printf("Resultado de la suma: ");
+    imprimir(&resultado2);
 
     return 0;
 }
