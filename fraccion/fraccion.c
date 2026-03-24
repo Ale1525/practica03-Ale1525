646f34616ee696554fca2fdc9e740e6d58d05fdf
Daniel Alejandro Gonzalez Morales <alejandrodaniel1525@gmail.com>
2026-03-24 03:15:24 +0000

    subo fraccion.c

--- fraccion/fraccion.c
+++ fraccion/fraccion.c
@@ -2,88 +2,46 @@
 #include <stdlib.h>
 #include "fraccion.h"
 
-/* TODO
-   Definir la estructura Fraccion
-   Debe contener:
-   - numerador
-   - denominador
-*/
-struct Fraccion
-{
-    /* TODO */
-};
-
-
-/* TODO
-   Crear una función auxiliar para calcular
-   el máximo común divisor (MCD)
-*/
-int mcd(int a, int b)
-{
-    /* TODO */
-
-    return 1;
+Fraccion crearfraccion(int numerador, int denominador) {
+    if(denominador == 0) {
+        printf("No existen fracciones con denominador 0\n");
+        denominador = 1; // Asignar un valor por defecto para evitar división por cero
+    }
+    Fraccion f;
+    f.numerador = numerador;
+    f.denominador = denominador;
+    return simplificar(&f);
 }
 
-
-/* Crear fracción */
-Fraccion* crearFraccion(int num, int den)
-{
-    /* TODO
-       1 Verificar que el denominador no sea 0
-       2 Reservar memoria con malloc
-       3 Asignar numerador y denominador
-       4 Simplificar la fracción
-       5 Regresar la fracción
-    */
-
-    return NULL;
+Fraccion simplificar(Fraccion *a) {
+    int num = a->numerador;
+    int den = a->denominador;
+    int mcd = 1;
+    for (int i = 1; i <= num && i <= den; i++) {
+        if (num % i == 0 && den % i == 0) {
+            mcd = i;
+        }
+    }
+    Fraccion resultado;
+    resultado.numerador = num / mcd;
+    resultado.denominador = den / mcd;
+    return resultado;
 }
 
-
-/* Simplificar fracción */
-void simplificar(Fraccion* f)
-{
-    /* TODO
-       1 Calcular el MCD
-       2 Dividir numerador y denominador entre el MCD
-    */
+Fraccion suma(Fraccion *a, Fraccion *b) {
+    Fraccion resultado;
+    resultado.numerador = a->numerador * b->denominador + a->denominador * b->numerador;
+    resultado.denominador = a->denominador * b->denominador;
+    return simplificar(&resultado);
 }
 
-
-/* Sumar fracciones */
-Fraccion* sumar(Fraccion* a, Fraccion* b)
-{
-    /* TODO
-       1 Calcular numerador resultante
-
-          a/b + c/d =
-          (a*d + b*c) / (b*d)
-
-       2 Crear nueva fracción
-       3 Simplificar
-       4 Regresar resultado
-    */
-
-    return NULL;
+void imprimir(Fraccion *a) {
+    (a->denominador == 0)? printf("No existen fracciones con denominador 0\n") : printf("%d/%d\n", a->numerador, a->denominador);
 }
 
-
-/* Imprimir fracción */
-void imprimir(Fraccion* f)
-{
-    /* TODO
-       Imprimir en formato:
-
-       numerador/denominador
-    */
-}
-
-
-/* Liberar memoria */
-void destruir(Fraccion* f)
-{
-    /* TODO
-       Liberar memoria con free
-    */
+void eliminar(Fraccion *a) {
+    if(a != NULL) {
+        free(a);
+        a = NULL;
+    }
 }
